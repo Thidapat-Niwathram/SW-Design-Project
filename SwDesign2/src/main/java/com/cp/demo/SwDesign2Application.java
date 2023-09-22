b9f58dcbb75ffbfea9e2c6aac5f215b37413d564
Thidapat-Niwathram <thidapat.n@kkumail.com>
2023-09-22 21:45:01 +0700

    first commit

---
+++ SwDesign2/src/main/java/com/cp/demo/SwDesign2Application.java
@@ -0,0 +1,13 @@
+package com.cp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwDesign2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwDesign2Application.class, args);
+	}
+
+}
