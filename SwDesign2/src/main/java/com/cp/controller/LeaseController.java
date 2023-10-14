48e5fe1a33f041153183ac07f8cc565b8acac799
parinyap03 <parinyaporn.sa@kkumail.com>
2023-10-14 19:24:01 +0700

    add delete in resident owner page

--- SwDesign2/src/main/java/com/cp/controller/LeaseController.java
+++ SwDesign2/src/main/java/com/cp/controller/LeaseController.java
@@ -1,11 +1,14 @@
 package com.cp.controller;
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cp.model.Lease;
+import com.cp.model.Resident;
 import com.cp.service.LeaseService;
 
 @Controller
@@ -20,10 +23,17 @@ public class LeaseController {
 	}
 
 	@GetMapping("/residents-owner")
-	    public String showAllLease(Model model) {
-	        List<Lease> lease = leaseService.getAllLeaseRepository();
-	        model.addAttribute("lease", lease);
-	        return "residents-owner";
+	public String showAllLease(Model model) {
+		List<Lease> lease = leaseService.getAllLeaseRepository();
+		model.addAttribute("lease", lease);
+		return "residents-owner";
 
-	    }
+	}
+
+	@GetMapping("/delete_resident/{id}")
+	public String deleteResident(@PathVariable("id") Integer id, Model model) {
+		Lease lease = leaseService.getLeaseById(id);
+		leaseService.deleteLeaseById(id);
+		return "redirect:/residents-owner";
+	}
 }
