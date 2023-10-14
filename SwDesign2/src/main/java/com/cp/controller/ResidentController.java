496bc285661c2c91682fb02667b60706a10beadf
“Rungtawan <rungtawan.s@kkumail.com>
2023-10-14 15:16:04 +0700

    add html and css

--- SwDesign2/src/main/java/com/cp/controller/ResidentController.java
+++ SwDesign2/src/main/java/com/cp/controller/ResidentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -27,6 +28,8 @@ public class ResidentController {
 		List<Resident> resi = residentService.getAllResident();
 		return resi;
 	}
+	
+	
 
 
 }
